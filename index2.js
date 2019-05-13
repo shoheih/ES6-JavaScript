bca3425a54e4e467d339e06dcd00a0808f4f3dea
shoheih <shoheigithub@gmail.com>
2019-05-14 04:08:44 +0900

    learn filter

---
+++ index2.js
@@ -0,0 +1,14 @@
+var products = [
+    { name: 'きゅうり', type: '野菜', quantity: 0, price: 1 },
+    { name: 'バナナ', type: 'フルーツ', quantity: 0, price: 15 },
+    { name: 'セロリ', type: '野菜', quantity: 30, price: 9 },
+    { name: 'オレンジ', type: 'フルーツ', quantity: 3, price:5 },
+];
+
+let filteredProduts = products.filter(function(product) {
+    return product.type == '野菜'
+        && product.quantity > 0
+        && product.price < 10;
+});
+
+console.log({filteredProduts});
