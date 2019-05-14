25dbeedbc90a85e5eba58d2c66a419b44e20a32d
shoheih <shoheigithub@gmail.com>
2019-05-15 03:51:52 +0900

    learn const and let

--- index3.js
+++
@@ -1,9 +0,0 @@
-function balancedParens(string) {
-    return !string.split('').reduce(function(previous, char) {
-        if (previous < 0) { return previous; }
-        if (char === '(') { return previous + 1; }
-        if (char === ')') { return previous - 1; }
-    }, 0);
-}
-
-console.log(balancedParens(')('));
